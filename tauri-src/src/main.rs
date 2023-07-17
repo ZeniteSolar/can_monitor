a71f22f4ec6012e0300303ae54d7667f43b57470
João Antônio Cardoso <joao.maker@gmail.com>
2023-07-17 01:06:28 -0300

    tauri-src: Clean up dependencies

--- tauri-src/src/main.rs
+++ tauri-src/src/main.rs
@@ -1,8 +1,6 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-#[macro_use]
-extern crate lazy_static;
 extern crate modular_bitfield;
 
 #[macro_use]
