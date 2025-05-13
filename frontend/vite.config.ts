59367525b02bad69c7349498a9a9e61145ab3f52
ayresgit <ayresg808@gmail.com>
2025-05-13 03:51:11 -0300

    Update commands for testing using npm run preview

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,11 +6,18 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+	  vue(),
   ],
   resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+	  alias: {
+		  '@': fileURLToPath(new URL('./src', import.meta.url))
+	  }
+  },
+  server: {
+	port: 3000
+  },
+  preview: {
+	  port: 4173, // standard
+	  open: true
   }
 })
