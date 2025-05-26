b9cf5b8e3ca421230cb3d82caa5a3b7b60dac236
ayresgit <ayres808@gmail.com>
2025-05-26 04:18:00 -0300

    ensure that there are always 2 cols

--- frontend/src/components/SwitchCard.vue
+++ frontend/src/components/SwitchCard.vue
@@ -1,33 +1,42 @@
 <template>
   <v-card class="ma-0 pa-0">
-    <v-card-title
-  :class="['py-0 mt-0 font-weight-black', titleColor ?? 'bg-primary text-black']"
-  :style="titleColor?.includes('text-black') ? 'color: black !important;' : ''"
->
-  {{ title }}
-</v-card-title>
+    <v-card-title :class="['py-0 mt-0 font-weight-black', titleColor ?? 'bg-primary text-black']"
+      :style="titleColor?.includes('text-black') ? 'color: black !important;' : ''">
+      {{ title }}
+    </v-card-title>
     <v-row class="px-2 ma-0">
-      <v-col
-        v-for="chunk in chunks"
-        :key="chunk[0].label"
-        :cols="12 / chunks.length"
-        class="ma-0 pa-1"
-      >
-        <v-switch
-          v-for="item in chunk"
-          :key="item.label"
-          :true-value="true"
-          :model-value="item.value"
-          :label="item.label"
-          color="red"
-          readonly
-          inset
-          hide-details
-        />
-      </v-col>
-    </v-row>
+  <v-col cols="6" class="ma-0 pa-1">
+    <v-switch
+      v-for="item in props.data.slice(0, Math.ceil(props.data.length / 2))"
+      :key="item.label"
+      :true-value="true"
+      :model-value="item.value"
+      :label="item.label"
+      color="red"
+      readonly
+      inset
+      hide-details
+    />
+  </v-col>
+  <v-col cols="6" class="ma-0 pa-1">
+    <v-switch
+      v-for="item in props.data.slice(Math.ceil(props.data.length / 2))"
+      :key="item.label"
+      :true-value="true"
+      :model-value="item.value"
+      :label="item.label"
+      color="red"
+      readonly
+      inset
+      hide-details
+    />
+  </v-col>
+</v-row>
+
   </v-card>
 </template>
+
+
 <script setup lang="ts">
 import { defineProps, computed } from 'vue';
 import type { BoardBoolean } from '@/types/index';
