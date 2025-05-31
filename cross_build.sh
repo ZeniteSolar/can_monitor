5811128d6ba3450201d49aa34c119a6a671948d5
ayresgit <ayres808@gmail.com>
2025-05-31 05:20:50 -0300

    better cross build

--- cross_build.sh
+++ cross_build.sh
@@ -1,3 +1,53 @@
 #!/usr/bin/env bash
 
-cross build --release --target=armv7-unknown-linux-gnueabihf
+set -e  # Exit on error
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Arguments
+REMOTE_HOST=$1
+if [ -z "$REMOTE_HOST" ]; then
+  echo "❌ ERROR: Please provide the target IP or hostname."
+  echo "Usage: $0 <ip-address-or-hostname>"
+  exit 1
+fi
+
+# Config
+TARGET=armv7-unknown-linux-gnueabihf
+BINARY_NAME=can_parser_rs
+BUILD_DIR=$PWD/target/$TARGET/release
+REMOTE_USER=zenite
+REMOTE_PATH="~/$BINARY_NAME"
+
+
+# Build frontend
+cd "$SCRIPT_DIR/frontend"
+echo "Building frontend…"
+npm run build
+
+cd ..
+# Build project
+cd "$SCRIPT_DIR"
+echo "📦 Building for target: $TARGET..."
+cross build --release --target=$TARGET
+echo "Build complete. Binary at: $BUILD_DIR/$BINARY_NAME"
+
+# Stop service
+echo "Stop service on Pi..."
+if ssh $REMOTE_USER@$REMOTE_HOST 'sudo systemctl stop simple_can_monitor.service'; then
+  echo "Service stopped successfully."
+  # Deploy
+  echo "Removing binary"
+  ssh $REMOTE_USER@$REMOTE_HOST "rm $REMOTE_PATH"
+  echo "Deploying to $REMOTE_USER@$REMOTE_HOST..."
+  scp "$BUILD_DIR/$BINARY_NAME" "$REMOTE_USER@$REMOTE_HOST:$REMOTE_PATH"
+
+  echo "reboot rasp"
+  ssh $REMOTE_USER@$REMOTE_HOST 'sudo reboot now'
+
+else
+  echo "❌ Failed to stop service."
+  exit 1
+fi
+
+echo "Done. Deployed to $REMOTE_USER@$REMOTE_HOST:$REMOTE_PATH"
